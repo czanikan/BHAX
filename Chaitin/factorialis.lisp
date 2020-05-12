1b4418a74e61c6d74e77199a0ffb6087106e2a1c
czanikan <32414135+czanikan@users.noreply.github.com>
2020-05-12 13:30:20 +0200

    Add files via upload

---
+++ Chaitin/factorialis.lisp
@@ -0,0 +1,28 @@
+#!/usr/bin/clisp
+
+(format t "Give a number: ~%")
+
+(setq n (read))
+
+(defun factorial_iterative (n)
+    (let ((f 1))
+        (dotimes (i n)
+        (setf f (* f (+ i 1))))
+        f 
+    )
+)
+
+(defun factorial_recursive (n)
+    (if (= n 0)
+        1
+        (* n (factorial_recursive(- n 1)))) )
+
+(format t "Recursive: ~%")
+
+(loop for i from 0 to n
+    do (format t "~D! = ~D~%" i (factorial_recursive i)))
+
+(format t "Iterative: ~%")
+
+(loop for i from 0 to n
+    do (format t "~D! = ~D~%" i (factorial_iterative i)))
